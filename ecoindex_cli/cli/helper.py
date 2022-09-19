18323b49bb8670791a326f5c80625439af78ef02
Vincent Vatelot <vincent.vatelot@ik.me>
2022-09-19 10:04:36 +0200

    ⬆️ chore(deps): Update deps (#175)
    * ⬆️ chore(deps): Update deps
    
      • Updating idna (3.3 -> 3.4)
      • Updating certifi (2022.6.15 -> 2022.9.14)
      • Updating pydantic (1.10.1 -> 1.10.2)
      • Updating sqlalchemy (1.4.40 -> 1.4.41)
      • Updating cryptography (37.0.4 -> 38.0.1)
      • Updating numpy (1.23.2 -> 1.23.3)
      • Updating sqlmodel (0.0.6 -> 0.0.7)
      • Installing contourpy (1.0.5)
      • Updating ecoindex (4.1.0 -> 5.0.0)
      • Updating fonttools (4.37.1 -> 4.37.2)
      • Updating pathspec (0.10.0 -> 0.10.1)
      • Updating pytest (7.1.2 -> 7.1.3)
      • Updating setuptools-scm (6.4.2 -> 7.0.5)
      • Updating twisted (22.4.0 -> 22.8.0)
      • Updating ecoindex-scraper (1.6.1 -> 2.3.0)
      • Updating matplotlib (3.5.3 -> 3.6.0)
    
    * ♻️ refact(ecoindex_scrap): Adapt New version of ecoindex_scrap

--- ecoindex_cli/cli/helper.py
+++ ecoindex_cli/cli/helper.py
@@ -1,8 +1,12 @@
 from asyncio import run
 
-from ecoindex_scraper import get_page_analysis
 from ecoindex_scraper.models import Result, WindowSize
+from ecoindex_scraper.scrap import EcoindexScraper
 
 
 def run_page_analysis(url: str, window_size: WindowSize) -> Result:
-    return run(get_page_analysis(url=url, window_size=window_size))
+    return run(
+        EcoindexScraper(url=url, window_size=window_size)
+        .init_chromedriver()
+        .get_page_analysis()
+    )
